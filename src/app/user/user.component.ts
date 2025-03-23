af89c85a6c0bf0aedf19d4b1cda5991ae22106f8
boualpso <cmtmoussa@gmail.com>
2025-03-23 03:04:06 +0200

    cleaned up some code and added another line with same users that are dynamic but via signals, UI bad, general Idea good

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
@@ -10,5 +10,32 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+
   selectedUser = DUMMY_USERS[randomIndex];
+  selectedUserSignal = signal( DUMMY_USERS[randomIndex]);
+
+  imagePathBySignal = computed(() => 'assets/users/'+this.selectedUserSignal().avatar)
+
+  get imagePath(){
+    return 'assets/users/'+ this.selectedUser.avatar
+
+  }
+
+
+  get namePath(){
+    return 'assets/user'+ this.selectedUser.name
+    
+  } 
+
+  onSelectedUser(){
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
+    
+  }
+
+  onSelectedUserSignal(){
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUserSignal.set(DUMMY_USERS[randomIndex]);
+    
+  }
 }
